b06e305769d61061d0388a64d0789c4cf47a7238
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-17 11:15:46 +0100

    creation app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>Intro</section>
+    <section>Parallax</section>
+    <section>A propos</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
