b87a70ec103a43ab17c19f8f150285519ebb39e5
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-17 13:06:00 +0100

    creation de la navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,16 @@ import "./navbar.scss"
 
  const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Laydi Soilihi</span>
+        <div className="social">
+          <a href="https://github.com/soilihilaydi"><img src="/github.png" alt=""/></a>
+          <a href="#"><img src="/Linkedin.png" alt=""/></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
