cdf7d32cfaaff946b936451740e6875ed06cf287
soilihi laydi <soilihi.laydi@gmail.com>
2024-01-23 19:48:49 +0100

    sidebar

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,4 +1,28 @@
+import { motion } from "framer-motion";
 
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
   const items = [
@@ -9,20 +33,21 @@ const Links = () => {
     "Contact",
   ]
 
-
-
-
-
-
   return (
-    <div className="links">{items.map((item) => (
-      <a href={`#${item}`}
-          key={item}>
-       {item}
-      </a>
-    ))}
-    </div>
-  )
-}
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
 
-export default Links
+export default Links;
