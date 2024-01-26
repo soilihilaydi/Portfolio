8c690612dfc98888914b36cd6a8ae9bcd5545de0
soilihi laydi <soilihi.laydi@gmail.com>
2024-01-26 15:54:13 +0100

    components hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,42 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>Laydi Soilihi </h2>
-          <h1>Développeur web full stack </h1>
-          <div className="buttons">
-            <button>Voir les dernières oeuvres </button>
-            <button>Contactez moi </button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
+        <motion.div className="textContainer" variants={textVariants} initial="initial"
+          animate="animate">
+          <motion.h2 variants={textVariants}>Laydi Soilihi </motion.h2>
+          <motion.h1 variants={textVariants}>Développeur web full stack </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>Voir les dernières oeuvres </motion.button>
+            <motion.button variants={textVariants}>Contactez moi </motion.button>
+          </motion.div>
+          <motion.img variants={textVariants}  src="/scroll.png" alt="" />
+        </motion.div>
       </div>
-
+      <div className="slidingTextContainer">
+        Créateur De Contenu Numérique
+      </div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
       </div>
