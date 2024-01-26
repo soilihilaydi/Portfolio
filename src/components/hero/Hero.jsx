7604e3b35ff66433549fac658686b0259dbf1e0b
soilihi laydi <soilihi.laydi@gmail.com>
2024-01-26 14:58:07 +0100

    components hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,6 +4,19 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="textContainer">
+        <h2>Laydi Soilihi </h2>
+        <h1>Développeur web full stack </h1>
+        <div className="buttons">
+          <button>Voir les dernières oeuvres </button>
+          <button>Contactez moi </button>
+        </div>
+        <img src="/scroll.png" alt="" />  
+      </div>
+
+
+
+
       <div className="imageContainer">
       <img src="/hero.png" alt="" />
       </div>
