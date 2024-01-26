2742b97090a21c5ffd79d87d7ff3fa10dfd25086
soilihi laydi <soilihi.laydi@gmail.com>
2024-01-26 15:09:28 +0100

    components hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,34 +1,25 @@
 import "./hero.scss";
 
-
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="textContainer">
-        <h2>Laydi Soilihi </h2>
-        <h1>Développeur web full stack </h1>
-        <div className="buttons">
-          <button>Voir les dernières oeuvres </button>
-          <button>Contactez moi </button>
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>Laydi Soilihi </h2>
+          <h1>Développeur web full stack </h1>
+          <div className="buttons">
+            <button>Voir les dernières oeuvres </button>
+            <button>Contactez moi </button>
+          </div>
+          <img src="/scroll.png" alt="" />
         </div>
-        <img src="/scroll.png" alt="" />  
       </div>
 
-
-
-
       <div className="imageContainer">
-      <img src="/hero.png" alt="" />
+        <img src="/hero.png" alt="" />
       </div>
     </div>
   );
 };
-
-
-
-
-
-
-
 
 export default Hero;
