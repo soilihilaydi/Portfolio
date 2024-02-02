d37dae21ef7aa1b80c96fecfa05b2d153dfc8e30
soilihi laydi <soilihi.laydi@gmail.com>
2024-02-02 14:14:43 +0100

    page contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,34 @@ import "./contact.scss"
 
 const Contact = () => {
   return (
-    <div className="contact">Contact</div>
+    <div className="contact">
+      <div className="textContainer">
+        <h1>toujours chaud pour taffer</h1>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>soilihi.laydi.pro@gmail.com</span>
+        </div>
+        <div className="item">
+          <h2>Adresse</h2>
+          <span>3 rue dareyre <br/>
+             07270 Lamastre
+          </span>
+        </div>
+        <div className="item">
+          <h2>Telephone</h2>
+          <span>07.67.36.00.68</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Va s'y envoye</button>
+        </form>
+      </div>
+    </div>
+     
   )
 }
 
