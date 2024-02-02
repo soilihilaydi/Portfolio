277bf2567aa7386d85c8876a46163b267f62d971
soilihi laydi <soilihi.laydi@gmail.com>
2024-02-02 21:57:39 +0100

    mise a niveaux

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,11 +12,11 @@ import { motion } from "framer-motion";
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}>
-          The Weboflady 
+          The Weboflaydi 
         </motion.span>
         <div className="social">
           <a href="https://github.com/soilihilaydi"><img src="/github.png" alt="lien vers github"/></a>
-          <a href="#"><img src="/Linkedin.png" alt="lien vers linkedin"/></a>
+          
         </div>
       </div>
     </div>
